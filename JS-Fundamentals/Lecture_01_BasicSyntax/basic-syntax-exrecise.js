cd38ccc18bb592ba007f757afd5afc6e812956bf
Zheko Ilchev <Jeko.Ilchev@gmail.com>
2026-02-22 23:56:49 +0200

    Add Task 1 - Basic Syntax - exercise.

---
+++ JS-Fundamentals/Lecture_01_BasicSyntax/basic-syntax-exrecise.js
@@ -0,0 +1,20 @@
+// Task 1.
+function print(input) {
+    let age;
+
+    if (input >= 0 && input <= 2) {
+        age = 'baby';
+    } else if (input >= 3 && input <= 13) {
+        age = 'child';
+    } else if (input >= 14 && input <= 19) {
+        age = 'teenager';
+    } else if (input >= 20 && input <= 65) {
+        age = 'adult';
+    } else if (input >= 66) {
+        age = 'elder';
+    } else {
+        age = 'out of bounds'
+    }
+
+    console.log(age);
+}
