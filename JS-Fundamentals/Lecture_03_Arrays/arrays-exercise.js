661134b1fe2d11e9507fa5aac88f20fc69cafad9
Zheko Ilchev <Jeko.Ilchev@gmail.com>
2026-08-25 23:34:25 +0300

    Arrays - exercise.

--- JS-Fundamentals/Lecture_03_Arrays/arrays-exercise.js
+++ JS-Fundamentals/Lecture_03_Arrays/arrays-exercise.js
@@ -1,5 +1,39 @@
 // Task 01.
-function printArray() {
-    let array = [1, 2, 3];
-    console.log(array.join(', '));
+function modifyArray(input) {
+    let originalSum = 0;
+    let modifiedSum = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    let currentElement = input[i];
+    originalSum += currentElement;
+
+    if (currentElement % 2 === 0) {
+      input[i] = currentElement + i;
+    } else {
+      input[i] = currentElement - i;
+    }
+
+    modifiedSum += input[i];
+  }
+
+  console.log(`[ ${input.join(", ")} ]`);
+  console.log(originalSum);
+  console.log(modifiedSum);
 }
+
+// modifyArray([5, 15, 23, 56, 35]);
+
+// Task 02.
+function findCommonElements(arr1, arr2) {
+    for (let element1 of arr1) {
+        for (let element2 of arr2) {
+            if (element1 === element2) {
+                console.log(element1);
+            }
+        }
+    }
+}
+
+// findCommonElements(
+// ['S', 'o', 'f', 't', 'U', 'n', 'i', ' '], 
+// ['s', 'o', 'c', 'i', 'a', 'l']);
